f5eea6a70679331a1d4b7182cb171473aefb0be4
SiyaChaudhary1107 <choudharysiya13@gmail.com>
2026-09-09 10:24:49 +0530

    Add multiple files

---
+++ promisesthencatch.js
@@ -0,0 +1,33 @@
+const fs = require("fs").promises;
+
+async function writeFile() {
+    try {
+        await fs.writeFile('section1.txt', 'Life is good', 'utf8');
+        console.log('File written successfully.');
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+writeFile();
+ async function readFile() {
+    try {
+        const data = await fs.readFile('section1.txt',  'utf8');
+        console.log('File Content.');
+        console.log(data);
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+readFile();
+
+//update
+ async function appendFile() {
+    try {
+        await fs.appendFile('section1.txt', "\nWelcome to FSD");
+        console.log('File Content.');
+        console.log("Data append successfully");
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+appendFile();
